7abe1f308a0295f629b55c86a74e1df598ae2925
SamGombs20 <gombajosh18@gmail.com>
2026-01-07 13:08:43 +0300

    Update:Setting up sign up form

---
+++ app/ui/SignUpForm.tsx
@@ -0,0 +1,10 @@
+export const SignUpForm =()=>{
+    return(
+        <form>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input id="name" name="name"  />
+            </div>
+        </form>
+    );
+}
